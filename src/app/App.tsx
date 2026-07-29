c1b9707d8f9434143f36e35b0933df9402d85e91
shane-braganca <braganza.shane@gmail.com>
2026-07-29 12:51:23 +0530

    Fixed issue

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
-import SplashScreen from "./components/ui/Splashscreen";
+import SplashScreen from "./components/Splashscreen";
 
 export default function App() {
   const [showSplash, setShowSplash] = useState(true);
